7cd382b2c80a3cfdf046a6beedcbed386e903fce
juanp3053 <juanp3053@gmail.com>
2018-03-19 08:24:37 -0400

    .

---
+++ Code2/InsulinA/src/com/RIS/controller/DoctorPageController.java
@@ -0,0 +1,188 @@
+package com.RIS.controller;
+
+import application.RISDbConfig;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import com.RIS.model.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+public class DoctorPageController {
+	
+	//configure the Patient table view
+		@FXML private TableView<Patient> tableViewInfo;
+	    @FXML private TableColumn<Patient, String> cPatientId;
+	    @FXML private TableColumn<Patient, String> cFirstName;
+	    @FXML private TableColumn<Patient, String> cLastName;
+	    @FXML private TableColumn<Patient, String> cAddress;
+	    @FXML private TableColumn<Patient, String> cBloodType;
+	    @FXML private TableColumn<Patient, String> cAge;
+	    @FXML private TableColumn<Patient, String> cGender;
+	    @FXML private TableColumn<Patient, String> cHeight;
+	    @FXML private TableColumn<Patient, String> cWeight;
+	    @FXML private TableColumn<Patient, String> cInsulinType;
+	    @FXML private TableColumn<Patient, String> cPhone;
+	    @FXML private TableColumn<Patient, String> cDoctor;
+		String ID;
+
+	    @FXML private Button addPatient, bDeletePatient, patientRecords;
+	
+	@FXML public void initialize() {
+		
+        //set up the columns in the table
+		cPatientId.setCellValueFactory(new PropertyValueFactory<Patient, String>("idPatient"));
+		cFirstName.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstName"));
+		cLastName.setCellValueFactory(new PropertyValueFactory<Patient, String>("lastName"));
+		cAddress.setCellValueFactory(new PropertyValueFactory<Patient, String>("address"));
+		cBloodType.setCellValueFactory(new PropertyValueFactory<Patient, String>("bloodType"));
+		cAge.setCellValueFactory(new PropertyValueFactory<Patient, String>("age"));
+		cGender.setCellValueFactory(new PropertyValueFactory<Patient, String>("gender"));
+		cHeight.setCellValueFactory(new PropertyValueFactory<Patient, String>("height"));
+		cWeight.setCellValueFactory(new PropertyValueFactory<Patient, String>("weight"));
+		cInsulinType.setCellValueFactory(new PropertyValueFactory<Patient, String>("insulinType"));
+		cPhone.setCellValueFactory(new PropertyValueFactory<Patient, String>("phone"));
+		cDoctor.setCellValueFactory(new PropertyValueFactory<Patient, String>("idDoctor"));
+		
+		tableViewInfo.setItems(getPatientList());
+		tableViewInfo.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        this.bDeletePatient.setDisable(true);
+        this.patientRecords.setDisable(true);
+	}
+	
+	// This method removes one or more selected row(s) from the TableView
+    public void deleteButtonPushed(){
+    	
+        ObservableList<Patient> selectedRows, allPatient;
+        allPatient = tableViewInfo.getItems();
+
+        //this gives us the rows that were selected
+        selectedRows = tableViewInfo.getSelectionModel().getSelectedItems();
+
+        //loop over the selected rows and remove the Patient objects from the table
+        for (Patient patient: selectedRows)
+        {
+        	allPatient.remove(patient);
+        	String query = "DELETE FROM patient where idPatient = ?;";
+
+        	try(
+        	    Connection conn = RISDbConfig.getConnection();
+        	    PreparedStatement updateprofile = conn.prepareStatement(query);
+        	){
+        		updateprofile.setString(1, patient.getidPatient());
+        	    updateprofile.executeUpdate();
+        	} catch (Exception e) {
+    			System.out.println("Status: operation failed due to "+e);
+
+    		}
+        }
+    }
+    
+ // ObservableList: A list that enables listeners to track changes when they occur
+    // The following  method will return an ObservableList of  object
+    public ObservableList<Patient>  getPatientList(){
+    	
+    	ObservableList<Patient> patient = FXCollections.observableArrayList();
+
+    	//idPatient, firstName, lastName, Address, bloodType, age, gender, height, weight, insulinType, phone, idDoctor
+        String SQLQuery = "SELECT * FROM patient ORDER BY idPatient ASC;"; //ADD WHERE idPatient == ''
+       	ResultSet rs = null;
+
+       	try(
+       			Connection conn = RISDbConfig.getConnection();
+       			PreparedStatement displayprofile = conn.prepareStatement(SQLQuery);
+       	){
+       		//displayprofile.setInt(1, cutomerId);
+       		rs = displayprofile.executeQuery();
+       		// check to see if receiving any data
+       		while (rs.next()){
+       			patient.add(new Patient(rs.getString("idPatient").toString(),rs.getString("firstName").toString(),rs.getString("lastName").toString(),rs.getString("Address").toString(),rs.getString("bloodType").toString(),
+       					rs.getString("age").toString(),rs.getString("gender").toString(),rs.getString("height").toString(),rs.getString("weight").toString(),rs.getString("insulinType").toString(),rs.getString("phone").toString(),rs.getString("idDoctor").toString()));
+       		}
+       	}catch(SQLException ex){
+       		RISDbConfig.displayException(ex);
+       		return null;
+       	}finally{
+       		if(rs != null){
+       			//rs.close();
+       		}
+       	}
+        return patient;
+    }
+    
+    // Method used to enable the detailed view button on mouse click event
+    public void mouseClickedOnTableView(){
+         	this.bDeletePatient.setDisable(false);
+         	this.patientRecords.setDisable(false);
+       }
+	
+	@FXML
+	public void changeSceneToPatientInsert(ActionEvent event) throws IOException {
+			if(event.getSource() == addPatient) {
+		    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		    window.setTitle("Insulin Pump");
+		    window.getIcons().add(new Image("/com/InsulinPump/images/blueHeartbeat.png"));
+			Parent tableViewParent = FXMLLoader.load(getClass().getResource("../../../com/InsulinPump/view/AddPatient.fxml"));
+		    Scene tableViewScene = new Scene(tableViewParent);
+	        window.setScene(tableViewScene);
+	        window.show();
+	        }
+	}
+	public void setID(String setID) {
+		ID = setID;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void changeSceneToPatientRecords() throws IOException {
+       
+		ObservableList<Patient> selectedRows, allPatient;
+        allPatient = tableViewInfo.getItems();
+
+        //this gives us the rows that were selected
+        selectedRows = tableViewInfo.getSelectionModel().getSelectedItems();
+        
+        for (Patient patient: selectedRows)
+        {
+            setID(patient.getidPatient());
+        }
+        
+        //Changes Panels and Passes USER ID for Query
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../com/InsulinPump/view/UserData.fxml"));
+		Parent root = (Parent) loader.load();
+		
+		UserDataController controllers = loader.getController();
+		controllers.setID(getID());
+		Stage stage = new Stage();
+		stage.setTitle("Insulin Pump");
+		stage.getIcons().add(new Image("/com/InsulinPump/images/blueHeartbeat.png"));
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ Code2/InsulinA/src/com/RIS/controller/UserDataController.java
@@ -0,0 +1,92 @@
+package com.RIS.controller;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.RIS.model.Record;
+import com.jfoenix.controls.JFXButton;
+
+import application.RISDbConfig;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+
+public class UserDataController {
+	
+	@FXML private AnchorPane paneUserData;
+	@FXML private TableView<Record> tblPatientData;
+	@FXML private TableColumn<Record, String> clnRecordId;
+	@FXML private TableColumn<Record, String> clnReadingTime;
+	@FXML private TableColumn<Record, String> clnGlucoseLevel;
+	@FXML private TableColumn<Record, String> clnInsulinAdmin;
+	@FXML private TableColumn<Record, String> clnStatus;
+	@FXML private JFXButton btnPatientMainMenu;
+	String IDP;
+	
+   
+ // ObservableList: A list that enables listeners to track changes when they occur
+    // The following  method will return an ObservableList of  object
+    public ObservableList<Record>  getRecordList(){
+    	
+    	ObservableList<Record> record = FXCollections.observableArrayList();
+        String SQLQuery = "SELECT * FROM records WHERE idPatient = ? ORDER BY idRecords DESC;"; //ADD WHERE idPatient = ''
+       	ResultSet rs = null;
+
+       	try(
+       			Connection conn = RISDbConfig.getConnection();
+       			PreparedStatement displayprofile = conn.prepareStatement(SQLQuery);
+       	){
+       		displayprofile.setString(1, IDP);
+       		rs = displayprofile.executeQuery();
+       		// check to see if receiving any data
+       		while (rs.next()){
+       			record.add(new Record(rs.getString("idRecords").toString(),rs.getString("dateTime").toString(),rs.getString("glucoseReading").toString(),rs.getString("insulinAmount").toString(),
+       					rs.getString("status").toString(),rs.getString("idPatient").toString(),rs.getString("idDoctor").toString()));
+       		}
+       	}catch(SQLException ex){
+       		RISDbConfig.displayException(ex);
+       		return null;
+       	}finally{
+       		if(rs != null){
+       			//rs.close();
+       		}
+       	}
+        return record;
+    }
+    
+    // Method used to enable the detailed view button on mouse click event
+	@FXML
+	public void changeSceneToPatientHome(ActionEvent event) throws IOException {
+		    Stage stage = (Stage)btnPatientMainMenu.getScene().getWindow();
+		    stage.close();
+		}
+	       
+	
+	
+	public void setID(String setID){
+		IDP = setID;
+		
+        //set up the columns in the table
+		clnRecordId.setCellValueFactory(new PropertyValueFactory<Record, String>("idRecords"));
+		clnReadingTime.setCellValueFactory(new PropertyValueFactory<Record, String>("dateTime"));
+		clnGlucoseLevel.setCellValueFactory(new PropertyValueFactory<Record, String>("glucoseReading"));
+		clnInsulinAdmin.setCellValueFactory(new PropertyValueFactory<Record, String>("InsulinAmount"));
+		clnStatus.setCellValueFactory(new PropertyValueFactory<Record, String>("Status"));	
+		tblPatientData.setItems(getRecordList());
+		
+		tblPatientData.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	}	
+	
+	
+	}
