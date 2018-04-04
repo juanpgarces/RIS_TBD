f594bb8f6db0ad46a2c96d0a30a42826ca559333
juanp3053 <juanp3053@gmail.com>
2018-04-04 17:43:02 -0400

    more

---
+++ Code/RIS/src/com/RIS/controller/AddNewAppointment.java
@@ -0,0 +1,20 @@
+package com.RIS.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AddNewAppointment {
+
+    @FXML private DatePicker txtDate; 
+    @FXML private TextField txtTime, txtFirstName, txtLastName, txtId;
+    @FXML private TextArea txtNotes;
+    @FXML private ComboBox<?> comboModality;
+    
+    public void createAppointment(ActionEvent event){
+    	
+    }	
+}
