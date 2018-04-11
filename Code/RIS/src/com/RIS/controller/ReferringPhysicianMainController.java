200185adcd41c2978d7425b748d4df7366cd50cd
jake.peavy <japeav1230@ung.edu>
2018-04-11 16:32:18 -0400

    ref physician

--- Code/RIS/src/com/RIS/controller/ReferringPhysicianMainController.java
+++ Code/RIS/src/com/RIS/controller/ReferringPhysicianMainController.java
@@ -1,5 +1,52 @@
 package com.RIS.controller;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.RIS.model.User;
+
+import application.RISDbConfig;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
 public class ReferringPhysicianMainController {
+	
+	 @FXML
+	    private TableColumn<?, ?> txtId;
+
+	    @FXML
+	    private TableColumn<?, ?> txtFirstName;
+
+	    @FXML
+	    private TableColumn<?, ?> txtLastName;
+
+	    @FXML
+	    private TableColumn<?, ?> txtGender;
+
+	    @FXML
+	    private TableColumn<?, ?> txtDob;
+
+	    @FXML
+	    private TableColumn<?, ?> modalityID;
+
+	    @FXML
+	    private Button buttonNewOrder;
+
+	    @FXML
+	    private Button buttonLogOut;
+
+	    @FXML
+	    private Button buttonNewPatient;
 
 }
